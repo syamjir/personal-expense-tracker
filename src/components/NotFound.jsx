eaf587ddd9f3d5f32ff6966f603c588cdbc84638
syamjir <rsyamjir@gmail.com>
2024-08-16 14:31:50 +0530

    Add NotFound component with styles

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./NotFound.module.css";
+function NotFound() {
+  return (
+    <div className={styles.notfound}>
+      NotFound<span>:(</span>
+    </div>
+  );
+}
+
+export default NotFound;
