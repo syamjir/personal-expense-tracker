3cc812e3a1d10846b41691104e7d8a5cb97dbde4
syamjir <rsyamjir@gmail.com>
2024-08-17 16:39:57 +0530

    Add Income component

---
+++ src/components/Income.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import TransactionPage from "../TransactionPage";
+
+function Income({ transactions, onSetTransactions, onHandleDelete }) {
+  const incomeLogs = transactions
+    ? transactions.filter((transaction) => transaction.type === "Credit")
+    : [];
+  const totalAmount = transactions
+    ? incomeLogs.reduce((acc, transaction) => acc + transaction.amount, 0)
+    : 0;
+  return (
+    <TransactionPage
+      type="Credit"
+      title="Incomes"
+      totalAmount={totalAmount}
+      btnContent="income"
+      transactions={incomeLogs}
+      onSetTransactions={onSetTransactions}
+      onHandleDelete={onHandleDelete}
+    />
+  );
+}
+
+export default Income;
