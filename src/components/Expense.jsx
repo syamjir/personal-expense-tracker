fbc948928b902113d5ce9c5904dfbb131050cb94
syamjir <rsyamjir@gmail.com>
2024-08-18 12:16:48 +0530

    Add Expense component

---
+++ src/components/Expense.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import TransactionPage from "../TransactionPage";
+
+function Expense({ transactions, onSetTransactions, onHandleDelete }) {
+  const expenseLogs = transactions
+    ? transactions.filter((transaction) => transaction.type === "Debit")
+    : [];
+  const totalAmount = transactions
+    ? expenseLogs.reduce((acc, transaction) => acc + transaction.amount, 0)
+    : 0;
+  return (
+    <TransactionPage
+      type="Debit"
+      title="Expenses"
+      totalAmount={totalAmount}
+      btnContent="expense"
+      transactions={expenseLogs}
+      onSetTransactions={onSetTransactions}
+      onHandleDelete={onHandleDelete}
+    />
+  );
+}
+
+export default Expense;
