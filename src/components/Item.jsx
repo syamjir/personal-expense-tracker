9129174e66b21e79a013c29451484234724c9b65
syamjir <rsyamjir@gmail.com>
2024-08-19 18:59:02 +0530

    Add item component

---
+++ src/components/Item.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FaEdit, FaCalendar, FaComment, FaTrash } from "react-icons/fa";
+import styles from "./Item.module.css";
+function Item({
+  colorDot,
+  colorText,
+  amount,
+  title,
+  date,
+  summary,
+  id,
+  onHandleDelete,
+  onEditTransaction,
+}) {
+  return (
+    <div className={styles.item}>
+      <div className={styles.container}>
+        <div className="sub__container">
+          <div className={`dot ${colorDot}`}></div>
+          <p>{title}</p>
+        </div>
+        <FaEdit
+          className={styles.edit__icon}
+          onClick={() => onEditTransaction(id)}
+        />
+      </div>
+      <div className={styles.container}>
+        <div className={styles["item__details-info"]}>
+          <div className="sub__container">
+            <p>₹{amount}</p>
+          </div>
+          <div className="sub__container">
+            <FaCalendar />
+            <p>{date}</p>
+          </div>
+          <div className="sub__container">
+            <FaComment />
+            <p>{summary}</p>
+          </div>
+        </div>
+        <FaTrash
+          className={styles.delete__icon}
+          onClick={() => onHandleDelete(id)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Item;
